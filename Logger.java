8b85adc1971de9fbc5f6a947c411beb4f9f22c6d
Oliver Nagy <19269749@studentmail.ul.ie>
2023-02-24 17:06:19 +0000

    Logger implemented

--- Logger.java
+++ Logger.java
@@ -18,10 +18,15 @@ public class Logger implements Interceptor {
     @Override
     public void interceptMovieTitle(Context m) {
 
+        String title = m.getMovieName();
+        System.out.println("LOG: Movie Name: " + title);
+
     }
 
     @Override
     public void interceptGetPriceCode(Context m) {
+        int priceCode = m.GetPriceCode();
+        System.out.println("LOG: Movie Price Code: " + priceCode);
 
     }
 }
