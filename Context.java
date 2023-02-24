4e39186aa48042ad86037185421a21f6eb69958a
Oliver Nagy <19269749@studentmail.ul.ie>
2023-02-24 16:35:54 +0000

    Context improvement

--- Context.java
+++ Context.java
@@ -1,3 +1,16 @@
+
+import java.util.Objects;
+
 public class Context {
+    private Customer customer;
+    public Context(Customer customer) {
+        this.customer = Objects.requireNonNull(customer, "Customer cannot be null.");
+    }
+    
+    public Customer getCustomer() {
+        return customer;
+    }
+    
+   
     
 }
