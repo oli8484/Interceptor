997238b9927cf8ed342cd296e3e1071c66711a22
Oliver Nagy <19269749@studentmail.ul.ie>
2023-02-25 18:04:06 +0000

    Main class fix

--- Main.java
+++ Main.java
@@ -1,16 +1,42 @@
 
 public class Main {
-    public static void main(String[] args) {
+  
         
-       Movie movie = new Movie("Titanic", 0);
-      
+            public static void main(String[] args) {
+                // create a movie object
+                Movie movie = new Movie("The Matrix", Movie.NEW_RELEASE);
         
-        Context context = new Context(movie);
-        Dispatcher dispatcher = new Dispatcher();
-        Concrete Concrete = new Concrete(context);
-        dispatcher.attach(Concrete);
-
+                // create a context object
+                Context context = new Context(movie);
+        
+                // create a dispatcher object
+                Dispatcher dispatcher = new Dispatcher();
+        
+                // create a concrete interceptor object and attach it to the dispatcher
+                Concrete interceptor = new Concrete(context);
+                dispatcher.attach(interceptor);
+        
+                // intercept the movie title and print the result
+                dispatcher.interceptMovieTitle(context);
+                // expected output: "Intercepted: The Matrix"
+                System.out.println("Title: " + movie.getTitle());
+        
+                // intercept the price code and print the result
+                dispatcher.interceptGetPriceCode(context);
+                // expected output: "Price code: 0"
+                System.out.println("Price code: " + movie.getPriceCode());
+        
+                // detach the interceptor
+                dispatcher.detach(interceptor);
+        
+                // intercept the movie title again and print the result
+                dispatcher.interceptMovieTitle(context);
+                // expected output: "The Matrix"
+                System.out.println("Title: " + movie.getTitle());
+            }
+        
+        
       
        
     }
-}
+
