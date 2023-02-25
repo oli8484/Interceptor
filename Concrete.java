7fcf5db281eb614225c704e2af85898b4a7502e7
Oliver Nagy <19269749@studentmail.ul.ie>
2023-02-25 17:44:10 +0000

    Concrete Fix

--- Concrete.java
+++ Concrete.java
@@ -19,14 +19,30 @@ public class Concrete implements Interceptor {
     public void interceptMovieTitle(Context m) {
 
         String title = m.getMovieName();
-        System.out.println("Movie Name: " + title);
+
+        // Example of modifying the movie title:
+        if (title.startsWith("The ")) {
+            String newTitle = title.replaceFirst("The ", "");
+            m.setMovie(new Movie(newTitle, m.getMovie().getPriceCode()));
+            System.out.println("Movie title intercepted and modified: " + newTitle);
+        } else {
+            System.out.println("Movie title intercepted but not modified: " + title);
+        }
 
     }
 
     @Override
     public void interceptGetPriceCode(Context m) {
+
         int priceCode = m.GetPriceCode();
-        System.out.println("Movie Price Code: " + priceCode);
+
+        // Example of modifying the price code:
+        if (priceCode == Movie.REGULAR) {
+            m.setMovie(new Movie(m.getMovie().getTitle(), Movie.NEW_RELEASE));
+            System.out.println("Price code intercepted and modified: " + Movie.NEW_RELEASE);
+        } else {
+            System.out.println("Price code intercepted but not modified: " + priceCode);
+        }
 
     }
 }
